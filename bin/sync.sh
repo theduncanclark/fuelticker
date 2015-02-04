0c1f7e6f43bade4c869fb3122189b6be2784b7aa
Duncan Clark <mail@duncanclark.net>
2015-02-04 14:43:27 +0100

    Make sync script upload to Graun

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,1 +1,2 @@
-rsync -aP --delete ~/Dropbox/Sites/fuelticker/site/ clarke.dreamhost.com:duncanclark.net/demos/fuelticker
+# rsync -aP --delete ~/Dropbox/Sites/fuelticker/site/ clarke.dreamhost.com:duncanclark.net/demos/fuelticker
+s3cmd -c ~/.s3cfg-guardian sync --delete-removed -v -M -P --add-header=Cache-Control:max-age=0 site/ s3://gdn-cdn/embed/kiln/fuelticker/
